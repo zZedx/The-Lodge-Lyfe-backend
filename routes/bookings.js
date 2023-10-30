27e2c0309aed77f536e63873751e7bce5d83607e
zZedx <Kartikajmera890@gmail.com>
2023-10-30 22:52:16 +0530

    filter query

--- routes/bookings.js
+++ routes/bookings.js
@@ -5,15 +5,27 @@ const catchAsyncError = require('../middleWares/catchAsyncError')
 const Booking = require("../models/bookings")
 const Guest = require('../models/guests')
 
-router.get('/' , catchAsyncError(async(req,res)=>{
-    const  allBookings = await Booking.find().populate({
-        path: 'guest',
-        select: 'fullName email'
-      }).populate({
-        path: 'cabin',
-        select: 'name'
-      })
+router.get('/', catchAsyncError(async (req, res) => {
+  const { status } = req.query
+  if (status === "all") {
+    const allBookings = await Booking.find().populate({
+      path: 'guest',
+      select: 'fullName email'
+    }).populate({
+      path: 'cabin',
+      select: 'name'
+    })
     res.json(allBookings)
+  } else {
+    const filteredBookings = await Booking.find({status}).populate({
+      path: 'guest',
+      select: 'fullName email'
+    }).populate({
+      path: 'cabin',
+      select: 'name'
+    })
+    res.json(filteredBookings)
+  }
 }))
 
 module.exports = router
