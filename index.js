8f900ba10e684a2dfaa25a5f0ff906b3a494a409
zZedx <Kartikajmera890@gmail.com>
2023-11-06 16:40:47 +0530

    server status route

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ app.use(cors({
 }));
 app.use(cookieParser());
 
+app.get('/status' , (req, res) => {
+  res.status(200).json({message: "OK"})
+})
+
 app.use("/cabins", cabinRoutes)
 app.use('/settings', settingsRoutes)
 app.use('/bookings', bookingsRoutes)
