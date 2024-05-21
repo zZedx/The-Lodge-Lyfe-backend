c9c4a515f7eaf1fb2890f91723fcfcb8d0e493bd
zZedx <kartikajmera890@gmail.com>
2024-05-21 14:48:12 +0530

    cors update

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 
-const cabinRoutes = require("./routes/cabins")
-const settingsRoutes = require("./routes/settings")
-const bookingsRoutes = require("./routes/bookings")
+const cabinRoutes = require("./routes/cabins");
+const settingsRoutes = require("./routes/settings");
+const bookingsRoutes = require("./routes/bookings");
 const usersRoutes = require("./routes/users");
 
 const PORT = process.env.PORT || 3000;
@@ -22,26 +22,28 @@ mongoose
   });
 
 app.use(express.json());
-app.use(cors({
-  origin: process.env.FRONTEND_URL, // Replace with frontend's URL
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL_ADMIN, process.env.FRONTEND_URL_CLIENT],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
-app.get('/status' , (req, res) => {
-  res.status(200).json({message: "OK"})
-})
+app.get("/status", (req, res) => {
+  res.status(200).json({ message: "OK" });
+});
 
-app.use("/cabins", cabinRoutes)
-app.use('/settings', settingsRoutes)
-app.use('/bookings', bookingsRoutes)
-app.use('/users', usersRoutes)
+app.use("/cabins", cabinRoutes);
+app.use("/settings", settingsRoutes);
+app.use("/bookings", bookingsRoutes);
+app.use("/users", usersRoutes);
 
 app.use((err, req, res, next) => {
-  const { status = 500 } = err
-  if (!err.message) err.message = "Oh No, Something Went Wrong!"
-  res.status(status).json({ err, message: err.message })
-})
+  const { status = 500 } = err;
+  if (!err.message) err.message = "Oh No, Something Went Wrong!";
+  res.status(status).json({ err, message: err.message });
+});
 
 app.listen(PORT, () => {
   console.log("Listening");
